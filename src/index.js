492ff586fd3f5548a235b9051f6f171b18b6cff8
John Hardy <jh@lagado.com>
2018-08-01 23:38:05 +1000

    chore(files):add todos

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import './index.css';
-// import registerServiceWorker from './registerServiceWorker';
 import hyper from 'hyperhtml';
-import { App } from './App'
+import "todomvc-common/base.css";
+import "todomvc-app-css/index.css";
 
-// registerServiceWorker();
-// render`xxxxx`;
+import controller from './controllers/todo';
+import storage from './models/storage';
+import updateHeader from './views/header';
+import updateMain from './views/main';
+import updateFooter from './views/footer';
 
+const header = hyper();
+const main = hyper();
+const footer = hyper();
+const appRender = hyper(document.querySelector('.todoapp'));
 
-const render = hyper(document.getElementById('root'));
-App(render);
+controller.init(storage, todos => {
+	appRender`${[
+		updateHeader(header, todos),
+		updateMain(main, todos),
+		updateFooter(footer, todos)
+	]}`;
+});
+
+controller.update();
+
 
