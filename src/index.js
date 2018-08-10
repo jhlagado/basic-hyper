0ca55dad66d665449b54b96cf82e4516efffdec0
John Hardy <jh@lagado.com>
2018-08-10 22:38:10 +1000

    chore(www): custom elements

--- src/index.js
+++ src/index.js
@@ -1,42 +1,41 @@
-// import { hyper } from "hyperhtml";
+import { hyper } from "hyperhtml";
 
-// import { initApp } from './app.js';
+import { initApp } from './app.js';
 
-// import "todomvc-common/base.css";
-// import "todomvc-app-css/index.css";
+import "todomvc-common/base.css";
+import "todomvc-app-css/index.css";
 
-// initApp(hyper, window); 
-
+initApp(hyper, window); 
  
-class MyElement extends HTMLElement {
-    connectedCallback() {
-      this.textContent = 'hello';
-    }
-}
+// class MyElement extends HTMLElement {
+//     connectedCallback() {
+//       this.textContent = 'hello';
+//     }
+// }
    
-customElements.define('my-element', MyElement);
+// customElements.define('my-element', MyElement);
 
-class MyElement1 extends HTMLElement {
-    connectedCallback() {
-      this.textContent = 'hello1';
-    }
-}
-customElements.define('my-element1', MyElement1);
+// class MyElement1 extends HTMLElement {
+//     connectedCallback() {
+//       this.textContent = 'hello1';
+//     }
+// }
+// customElements.define('my-element1', MyElement1);
    
-import { MyElement2 } from './element';
-customElements.define('my-element2', MyElement2);
+// import { MyElement2 } from './element';
+// customElements.define('my-element2', MyElement2);
 
-import HyperHTMLElement from './common/hyperhtml-element';
+// import HyperHTMLElement from './common/hyperhtml-element';
 
-console.log(HyperHTMLElement);
+// console.log(HyperHTMLElement);
 
-class CustomElement extends HyperHTMLElement {
-    static get observedAttributes() { return ['owner']; }
-    created() { this.render(); }
-    render() {
-      this.html`
-      This is <b>${this.owner}</b>'s name-tag element.`;
-    }
-  }
+// class CustomElement extends HyperHTMLElement {
+//     static get observedAttributes() { return ['owner']; }
+//     created() { this.render(); }
+//     render() {
+//       this.html`
+//       This is <b>${this.owner}</b>'s name-tag element.`;
+//     }
+//   }
 
-CustomElement.define('custom-element');
+// CustomElement.define('custom-element');
