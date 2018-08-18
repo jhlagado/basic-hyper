a9ec5df5591656ccd50197c4bb77d8b81901d4ac
John Hardy <jh@lagado.com>
2018-08-18 22:34:06 +1000

    chore(www): added travis deployment and lighthouse cichore(www): added travis deployment and lighthouse ci

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import "todomvc-common/base.css";
 import "todomvc-app-css/index.css";
-import { initApp } from './app.js';
+
+import { initController } from './controllers/todo';
+import { initStorage } from './models/storage';
+import './elements';
 
 const hyper = HyperHTMLElement.hyper;
 
-initApp(hyper, window);
+const storage = initStorage(window.localStorage);
+const controller = initController(window, storage);
+const data = { controller, hyper };
+const appRender = hyper(window.document.querySelector('#todoapp'));
 
+appRender`<my-container data=${data}></my-container>`;
+controller.update(); 
