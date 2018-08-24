e1b49eb6b1fca27bed4d02653aef095ec5d09d01
John Hardy <jh@lagado.com>
2018-08-24 20:38:07 +1000

    updated event handlers

--- src/elements/todo-item.js
+++ src/elements/todo-item.js
@@ -6,42 +6,51 @@ export class TodoItem extends HyperHTMLElement {
 	
 	created() {
 		this.render();
+		// this.complete1.bind(this);
 	}
 	
 	attributeChangedCallback(name, oldValue, newValue) { 
 		this.render(); 
 	}	
 	
-	dblclick2Edit(event, input) {
+	dblclick2Edit(event) {
+		const input = event.target;
 		const li = input.closest('li');
 		li.classList.add('editing');
 		li.querySelector('.edit').focus();
 	}
 	
-	blur2Save(index, event, input) {
+	blur2Save(event) {
+		const index = this.getAttribute('index');
+		const input = event.target;
 		input.closest('li').classList.remove('editing');
 		this.data.controller.edit(index, event);
 	}
 
-	edit(index, event, input) {
+	edit(event) {
+		const index = this.getAttribute('index');
+		const input = event.target;
 		if (event.keyCode === ENTER_KEY) {			
 			input.closest('li').classList.remove('editing');
 		}
 		this.data.controller.edit(index, event);
 	}
 
-	escape2Reset(event, input) {
+	escape2Reset(event) {
+		const input = event.target;
 		if (event.keyCode === ESC_KEY) {
 			input.value = this.data.todo.title;
 			input.blur();
 		}
 	}
 
-	complete(index) {
+	complete(event) {
+		const index = this.getAttribute('index');
 		this.data.controller.complete(index);
 	}
 
-	destroy(index) {
+	destroy(event) {
+		const index = this.getAttribute('index');
 		this.data.controller.destroy(index);
 	}
 
@@ -49,11 +58,10 @@ export class TodoItem extends HyperHTMLElement {
 		const element = this;
 		const title = this.getAttribute('title');
 		const completed = this.getAttribute('completed');
-		const index = this.getAttribute('index');
 
 		return this.html`
 		<li
-			data-index="${index}"
+			data-index="${this.getAttribute('index')}"
 			class="${completed ? 'completed' : ''}"
 		>
 			<div class="view">
@@ -61,24 +69,21 @@ export class TodoItem extends HyperHTMLElement {
 					class="toggle"
 					type="checkbox"
 					checked="${completed}"
-					onclick="${() => element.complete(index)}">
-				<label ondblclick="${function(event){
-					element.dblclick2Edit(event, this);
-				}}">${title}</label>
-				<button class="destroy" onclick="${() => element.destroy(index)}"></button>
+					onclick="${event => this.complete(event)}">
+				<label 
+					ondblclick="${
+					event => this.dblclick2Edit(event)
+				}">${title}</label>
+				<button class="destroy" onclick="${
+					event => this.destroy(event)
+				}"></button>
 			</div>
 			<input
 				class="edit"
 				value="${title}"
-				onblur="${function(event){ 
-					element.blur2Save(index, event, this);
-				}}"
-				onkeypress="${function(event){
-					element.edit(index, event, this);
-				}}"
-				onkeydown="${function (event) {
-					element.escape2Reset(event, this);
-				}}">
+				onblur="${event => this.blur2Save(event)}"
+				onkeypress="${event => this.edit(event)}"
+				onkeydown="${event => this.escape2Reset(event)}">
 		</li>`;
 	}
 }
